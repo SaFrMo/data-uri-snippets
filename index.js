bc5c9a411e827c2bb9bcc199d6658c234c7cc695
Sander <sandermoolin@gmail.com>
2017-05-24 08:54:10 -0400

    Added 1x1 gif

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export default {
 
-    white1x1: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAAAAAA6fptVAAAACklEQVQYV2P4DwABAQEAWk1v8QAAAABJRU5ErkJggg=='
+    white1x1: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=',
+    white1x1png: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAAAAAA6fptVAAAACklEQVQYV2P4DwABAQEAWk1v8QAAAABJRU5ErkJggg=='
 
 }
